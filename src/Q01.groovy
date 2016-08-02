7a137ffdd9a2d7db35e0215c246be2baa5e3325f
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-02 21:23:01 +0100

    Now prints value of d1 & d2 - showing they're not equal

--- src/Q01.groovy
+++ src/Q01.groovy
@@ -10,7 +10,8 @@ if (d1 == d2) {
     println("1.255 is equal to 1.255 plus 7 minus 7")
 } else {
     println("1.255 is NOT equal to 1.255 plus 7 minus 7!")
+    println "because d1 = " + d1 + " and d2 = " + d2
     if (abs(d1 - d2) < 10E-6) {
-        println "But a further check confirms that it's pretty close - within 10E-6"
+        println "A further check confirms that it's pretty close - within 10E-6"
     }
 }
