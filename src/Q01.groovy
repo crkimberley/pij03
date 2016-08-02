dc41b0a8758aaacd1b870c7294ed953ba194f7e8
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-02 17:24:57 +0100

    Initial commit

---
+++ src/Q01.groovy
@@ -0,0 +1,16 @@
+import static java.lang.Math.abs
+
+/**
+ * Created by crk on 02/08/2016.
+ */
+double d1 = 1.255
+double d2 = d1 + 7 - 4 - 3
+print "According to the initial calculation in this Java program "
+if (d1 == d2) {
+    println("1.255 is equal to 1.255 plus 7 minus 7")
+} else {
+    println("1.255 is NOT equal to 1.255 plus 7 minus 7!")
+    if (abs(d1 - d2) < 10E-6) {
+        println "But a further check confirms that it's pretty close - within 10E-6"
+    }
+}
