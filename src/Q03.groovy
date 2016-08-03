52656b44745c7bfc12eccf3e5eb6281056c6601d
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-03 02:08:29 +0100

    Q03 - previous versions relied on spaces in input, this version doesn't want spaces

--- src/Q03.groovy
+++ src/Q03.groovy
@@ -3,20 +3,34 @@
  */
 Scanner input = new Scanner(System.in)
 double result, number1, number2
-String entry
+String entry, str1, str2
 char operation
+// char literals
+char minus = '-'
+char zero = '0'
+char nine = '9'
+char point = '.'
 boolean correctInput = true
-println "Command Line Calculator"
+println "Command Line Calculator - using entered expressions, eg. 4.87*96.2"
+println "To exit - enter a character other than + - * / as the operator"
 while (correctInput) {
-    println "Enter: No.1 Operation No.2"
+    str1 = ""
+    println "Enter expression"
     entry = input.nextLine()
-    Scanner expression = new Scanner(entry)
-    number1 = Double.parseDouble(expression.next())
-    println number1
-    operation = expression.next().charAt(0)
-    println operation
-    number2 = Double.parseDouble(expression.next())
-    println number2
+    for (int i=0; i < entry.length(); i++) {
+        char ch = entry.charAt(i)
+        // 1st number string - checking for digits, catering for negative numbers & decimal points
+        if ((i==0 && ch == minus) || (ch >= zero && ch <= nine) || (ch == point)) {
+            str1 = str1 + entry.charAt(i)
+        } else {
+            // mathematical operator followed by 2nd number string
+            operation = entry.charAt(i)
+            str2 = entry.substring(i+1, entry.length())
+            break
+        }
+    }
+    number1 = Double.parseDouble(str1)
+    number2 = Double.parseDouble(str2)
     switch (operation) {
         case '+':
             result = number1 + number2
@@ -31,10 +45,11 @@ while (correctInput) {
             result = number1 / number2
             break;
         default:
-            println "That's not a correct selection - program will terminate"
-            result = 0
+            println "That's not one of the 4 operators allowed - program will terminate"
             correctInput = false
             break;
     }
-    println number1 + " " + operation + " " + number2 + " = " + result
+    if (correctInput) {
+        println number1 + " " + operation + " " + number2 + " = " + result
+    }
 }
