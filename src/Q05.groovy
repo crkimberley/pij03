5f1d5bc1c20f5e2abcfcc393e110925f00034a03
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-03 13:13:04 +0100

    Q05 - initial commit, tested & working

---
+++ src/Q05.groovy
@@ -0,0 +1,25 @@
+/**
+ * Created by crk on 03/08/2016.
+ */
+class Rectangle {
+    Point upLeft
+    Point downRight
+}
+
+Scanner input = new Scanner(System.in)
+println "Enter X & Y coordinates for opposite corners of a rectangle"
+print "upLeft.x, upleft.y, downRight.x, downRight.y - with a space between each coordinate: "
+Scanner entry = new Scanner(input.nextLine())
+Point upLeft = new Point()
+upLeft.x = entry.nextDouble()
+upLeft.y = entry.nextDouble()
+Point downRight = new Point()
+downRight.x = entry.nextDouble()
+downRight.y = entry.nextDouble()
+Rectangle rectangle = new Rectangle()
+rectangle.upLeft = upLeft
+rectangle.downRight = downRight
+double width = rectangle.downRight.x - rectangle.upLeft.x
+double height = rectangle.upLeft.y - rectangle.downRight.y
+println "Perimeter = " + 2 * (width + height)
+println "Area = " + width * height
