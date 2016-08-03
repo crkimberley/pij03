d965ab0ce0c85c5086b0928a9c17801789952862
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-03 12:48:24 +0100

    Q04 - simplified, reducing duplicate code, now entering all coordinates at once

--- src/Q04.groovy
+++ src/Q04.groovy
@@ -12,17 +12,12 @@ Point pointA = new Point()
 Point pointB = new Point()
 Point pointC = new Point()
 Scanner input = new Scanner(System.in)
-Scanner entry
-println "Enter X & Y coordinates for Point A, with a space between them"
-entry = new Scanner(input.nextLine())
+print "Enter X & Y coordinates for Points A, B & C with a space between each coordinate: "
+Scanner entry = new Scanner(input.nextLine())
 pointA.x = entry.nextDouble()
 pointA.y = entry.nextDouble()
-println "Enter X & Y coordinates for Point B, with a space between them"
-entry = new Scanner(input.nextLine())
 pointB.x = entry.nextDouble()
 pointB.y = entry.nextDouble()
-println "Enter X & Y coordinates for Point C, with a space between them"
-entry = new Scanner(input.nextLine())
 pointC.x = entry.nextDouble()
 pointC.y = entry.nextDouble()
 double distanceSquaredAB = pow((pointA.x - pointB.x), 2) + pow((pointA.y - pointB.y), 2)
