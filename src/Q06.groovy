0a71dddbca31619974cb14c0e86ef61abeca1bb4
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-04 00:05:19 +0100

    Q06 - initial commit - working/tested

---
+++ src/Q06.groovy
@@ -0,0 +1,26 @@
+/**
+ * Created by crk on 03/08/2016.
+ */
+Scanner input = new Scanner(System.in)
+println "Point inside or outside a rectangle?"
+println "Enter X & Y coordinates for opposite corners of a rectangle and a 3rd point"
+print "upLeft.x, upleft.y, downRight.x, downRight.y, point.x, point.y - with a space between each coordinate: "
+Scanner entry = new Scanner(input.nextLine())
+Point upLeft = new Point()
+upLeft.x = entry.nextDouble()
+upLeft.y = entry.nextDouble()
+Point downRight = new Point()
+downRight.x = entry.nextDouble()
+downRight.y = entry.nextDouble()
+Rectangle rectangle = new Rectangle()
+rectangle.upLeft = upLeft
+rectangle.downRight = downRight
+Point point = new Point()
+point.x = entry.nextDouble()
+point.y = entry.nextDouble()
+if ((point.x >= rectangle.upLeft.x && point.x <= rectangle.downRight.x) &&
+        (point.y >= rectangle.downRight.y && point.y <= rectangle.upLeft.y)) {
+    println "That point is inside the rectangle"
+} else {
+    println "That point is outside the rectangle"
+}
