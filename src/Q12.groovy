0399787d839963f82298b6ea1038a68c2ef027b3
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-04 20:26:42 +0100

    Q12 - initial commit - basi change calculation

--- src/Q12.groovy
+++ src/Q12.groovy
@@ -1,5 +1,10 @@
 /**
  * @author crkimberley on 04/08/2016.
  */
-class Q12 {
-}
+Scanner input = new Scanner(System.in)
+println "Change calculator: enter total cost of purchase (as pounds.pence, eg. 3.50)"
+int costInPence = input.nextDouble() * 100
+println "Enter amount paid to buy it"
+int paidInPence = input.nextDouble() * 100
+int change = paidInPence - costInPence
+        println "Change = £" + (int) (change / 100) + "." + change % 100
