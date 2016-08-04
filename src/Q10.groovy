769af2ae765110e1d5dac19fe8960e0ce9288892
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-04 01:47:10 +0100

    Q10 - initial commit

---
+++ src/Q10.groovy
@@ -0,0 +1,10 @@
+/**
+ * Created by crk on 04/08/2016.
+ */
+Scanner input = new Scanner(System.in)
+println "Enter a short string and find out how many times it appears in a longer text"
+print "Enter a short string: "
+String shortString = input.nextLine()
+println "Enter text"
+String text = input.nextLine()
+
