5a7e5ade24460dfc71830986b6fdd2a4788f84e6
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-04 13:12:27 +0100

    Q10 - working/tested

--- src/Q10.groovy
+++ src/Q10.groovy
@@ -1,10 +1,25 @@
 /**
  * Created by crk on 04/08/2016.
  */
+// Only String methods allowed: charAt() & length()
+// Note - this implementation would count "bbb" 3 times in "bbbbb"
 Scanner input = new Scanner(System.in)
 println "Enter a short string and find out how many times it appears in a longer text"
-print "Enter a short string: "
+println "Enter a short string"
 String shortString = input.nextLine()
 println "Enter text"
 String text = input.nextLine()
-
+String stringCount = 0
+for (int i=0;i<text.length();i++) {
+    if (text.charAt(i) == shortString.charAt(0) && text.length() - i >= shortString.length()) {
+        for (int j=1;j<shortString.length();j++) {
+            if (text.charAt(i+j) != shortString.charAt(j)) {
+                break
+            }
+            if (j == shortString.length() - 1) {
+                stringCount++
+            }
+        }
+    }
+}
+println shortString + " appears " + stringCount + " times"
