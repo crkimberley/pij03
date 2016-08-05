3e7881ab176b928ba034e7f01701063ce5bebeb2
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-05 03:43:24 +0100

    Q16 - initial commit - partial solution

--- src/Q16.groovy
+++ src/Q16.groovy
@@ -5,3 +5,18 @@ Scanner input = new Scanner(System.in)
 println "Enter a number with commas & decimal point, eg. 23,419.34 and program will divide by 2"
 print "Enter number (text): "
 String text = input.nextLine()
+String numberString1 = ""
+String numberString2 = ""
+int pointPosition = 0nu
+for (int i=0;i<text.length();i++) {
+    if (text.charAt(i) != ',') {
+        if (text.charAt(i) != '.') {
+            numberString1 = numberString1 + text.charAt(i)
+        } else {
+            pointPosition = i
+            break
+        }
+    }
+}
+int wholeNumber = Integer.parseInt(numberString1)
+numberString2 = text.substring(pointPosition + 1)
