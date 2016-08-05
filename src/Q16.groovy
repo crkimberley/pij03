bef2dd2abfda5e234dff8a95cbff9b47ba865864
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-05 13:36:58 +0100

    Q16 - calculation refined and comments added - working/tested

--- src/Q16.groovy
+++ src/Q16.groovy
@@ -5,6 +5,8 @@ Scanner input = new Scanner(System.in)
 println "Enter a number with commas & decimal point, eg. 23,419.34 and program will divide by 2"
 print "Enter number (text): "
 String text = input.nextLine()
+
+// Whole number calculation
 String wholeNumberString = ""
 int pointPosition = 0
 for (int i=0;i<text.length();i++) {
@@ -19,11 +21,18 @@ for (int i=0;i<text.length();i++) {
 }
 int wholeNumber = Integer.parseInt(wholeNumberString)
 int wholeNumberResult = wholeNumber / 2
+
+// Decimal number calculation
 String decimalString = text.substring(pointPosition + 1)
-// Add digit to left-hand end to retain leading zeroes & include any remainder from whole number division
+// Add digit to left-hand end to retain leading zeroes & include remainder from whole number division
+// If there's a remainder add 3, otherwise 2. This digit will be removed after dividing by 2
 int remainder = wholeNumber % 2
 String remainderLeadString = "" + (remainder + 2)
-String adjustedDecimalString = remainderLeadString + decimalString
+// Add a zero to right-hand end to give correct result if final decimal digit is odd
+boolean finalDecimalDigitOdd = Integer.parseInt(text.substring(text.length() - 1)) % 2 == 1
+String finalZeroString = finalDecimalDigitOdd ? "0" : ""
+// Adding adjustment digits
+String adjustedDecimalString = remainderLeadString + decimalString + finalZeroString
 int adjustedDecimalNumber = Integer.parseInt(adjustedDecimalString)
 int adjustedDecimalResult = adjustedDecimalNumber / 2
 // Remove leading adjusting digit for decimal result
