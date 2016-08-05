4668a59f7596e427d3a1a50a5b121049e7b14ad1
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-05 13:12:03 +0100

    Q16 - initial commit - 1st solution - needs adjustment to decimal calculation - add zero to end to account for odd final digit

--- src/Q16.groovy
+++ src/Q16.groovy
@@ -5,18 +5,29 @@ Scanner input = new Scanner(System.in)
 println "Enter a number with commas & decimal point, eg. 23,419.34 and program will divide by 2"
 print "Enter number (text): "
 String text = input.nextLine()
-String numberString1 = ""
-String numberString2 = ""
-int pointPosition = 0nu
+String wholeNumberString = ""
+int pointPosition = 0
 for (int i=0;i<text.length();i++) {
     if (text.charAt(i) != ',') {
         if (text.charAt(i) != '.') {
-            numberString1 = numberString1 + text.charAt(i)
+            wholeNumberString = wholeNumberString + text.charAt(i)
         } else {
             pointPosition = i
             break
         }
     }
 }
-int wholeNumber = Integer.parseInt(numberString1)
-numberString2 = text.substring(pointPosition + 1)
+int wholeNumber = Integer.parseInt(wholeNumberString)
+int wholeNumberResult = wholeNumber / 2
+String decimalString = text.substring(pointPosition + 1)
+// Add digit to left-hand end to retain leading zeroes & include any remainder from whole number division
+int remainder = wholeNumber % 2
+String remainderLeadString = "" + (remainder + 2)
+String adjustedDecimalString = remainderLeadString + decimalString
+int adjustedDecimalNumber = Integer.parseInt(adjustedDecimalString)
+int adjustedDecimalResult = adjustedDecimalNumber / 2
+// Remove leading adjusting digit for decimal result
+String adjustedDecimalResultString = "" + adjustedDecimalResult
+String decimalResultString = adjustedDecimalResultString.substring(1)
+
+println text + " / 2 = " + wholeNumberResult + "." + decimalResultString
