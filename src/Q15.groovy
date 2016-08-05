24ca8215d666a081020e0744122e5372fb670ee4
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-05 02:46:42 +0100

    Q15 - initial commit - working/tested

--- src/Q15.groovy
+++ src/Q15.groovy
@@ -1,3 +1,22 @@
 /**
  * @author crkimberley on 04/08/2016.
  */
+Scanner input = new Scanner(System.in)
+print "Relaxed Palindrome checker - enter text: "
+text = input.nextLine()
+String cleanText = ""
+char textChar
+for (int i=0;i<text.length();i++) {
+    textChar = text.charAt(i)
+    if (textChar.isLetter()) {
+        cleanText = cleanText + textChar.toLowerCase()
+    }
+}
+boolean check = true
+for (int i=0;i<cleanText.length()/2;i++) {
+    if (cleanText.charAt(i) != cleanText.charAt(cleanText.length() - i - 1)) {
+        check = false
+        break
+    }
+}
+println "It is" + (check ? "" : " not") + " a palindrome"
