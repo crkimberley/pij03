820d2a3eae06a939ac8012015fea397ba5e2c2a8
Chris <ckimbe02@dcs.bbk.ac.uk>
2016-08-06 12:19:39 +0100

    Q17 -now reads body of email

--- src/Q17.groovy
+++ src/Q17.groovy
@@ -3,25 +3,57 @@
  */
 Scanner input = new Scanner(System.in)
 println "Mail Server Program"
-print ">>> "
-String fromCommand = input.nextLine()
-println "OK"
-print ">>> "
-String toCommand = input.nextLine()
-println "OK"
-print ">>> "
-String bodyCommand = input.nextLine()
-String body = ""
-String lineOfBody
-while (true) {
-    lineOfBody = input.nextLine()
-    if (lineOfBody == ".") {
+boolean quit = false
+
+// Email return address
+boolean done = false
+while (!done) {
+    print ">>> "
+    text = input.nextLine()
+    if (text == "QUIT") {
+        quit = true
         break
     }
-    body = body + lineOfBody + "\n"
+    done = true
+    println "OK"
 }
+
+// Email recipient
+done = false
+while (!done && !quit) {
+    print ">>> "
+    text = input.nextLine()
+    if (text == "QUIT") {
+        quit = true
+        break
+    }
+    done = true
+    println "OK"
+}
+
+// Body of email
+String body = ""
+done = false
+while (!done && !quit) {
+    print ">>> "
+    text = input.nextLine()
+    if (text == "QUIT") {
+        quit = true
+        break
+    }
+    if (text == "DATA") {
+        while (true) {
+            text = input.nextLine()
+            if (text == ".") {
+                break
+            }
+            body = body + text + "\n"
+        }
+        done = true
+    }
+}
+
 println "Sending email..."
 println "from: "
 println "to: "
 print body
-println ">>> "
